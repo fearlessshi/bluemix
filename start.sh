e2be3fcc818cf31e5a1929477a0d3b2aed31dbec
tprss <xiguaiyong@163.com>
2016-02-26 04:25:05 +0800

    去掉提示

--- start.sh
+++ start.sh
@@ -2,26 +2,17 @@
 
 # 安装依赖
 
-echo -e '开始安装docker。。。'
-sudo apt-get update >/dev/null 2>&1
-sudo apt-get install docker.io wget -y >/dev/null 2>&1
-echo -e '完毕\n'
+sudo apt-get update 
+sudo apt-get install docker.io wget -y 
 
-echo -e '开始安装cf命令。。。'
-wget -O cf.deb 'https://coding.net/u/tprss/p/bluemix-source/git/raw/master/cf-cli-installer_6.16.0_x86-64.deb' >/dev/null 2>&1
-sudo dpkg -i cf.deb >/dev/null 2>&1
-echo -e '完毕\n'
+wget -O cf.deb 'https://coding.net/u/tprss/p/bluemix-source/git/raw/master/cf-cli-installer_6.16.0_x86-64.deb' 
+sudo dpkg -i cf.deb 
 
-echo -e '开始安装ic插件。。。'
-cf install-plugin -f https://coding.net/u/tprss/p/bluemix-source/git/raw/master/ibm-containers-linux_x64 >/dev/null 2>&1
-echo -e '完毕\n'
+cf install-plugin -f https://coding.net/u/tprss/p/bluemix-source/git/raw/master/ibm-containers-linux_x64 
 
 # 初始化环境
-clear
-echo -e '登陆Bluemix账户中。。。\n'
 cf login -a https://api.ng.bluemix.net
-cf ic init >/dev/null 2>&1
-clear
+cf ic init 
 
 # 生成密码
 passwd=`openssl rand -base64 12`
@@ -39,17 +30,12 @@ EXPOSE 443
 CMD ["ssserver","-p","443","-k",${passwd},"-m","aes-256-cfb"]
 _EOF_
 
-echo -e '开始构建镜像。。。'
-cf ic build -t ss:v1 . >/dev/null 2>&1
-echo -e '完毕\n'
+cf ic build -t ss:v1 . 
 
 # 运行容器
-echo -e '创建SS容器。。。'
 cf ic run --name=ss -p 443 registry.ng.bluemix.net/`cf ic namespace get`/ss:v1
-echo -e '完毕\n'
 
 # 显示信息
-echo '容器启动中。。。'
 sleep 15
 clear
 echo -e "password:\n"${passwd}"\naddress:"
