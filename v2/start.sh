b60bf6400d68e6c78c956d9bb4e31f111807a92b
tprss <xiguaiyong@163.com>
2017-07-22 22:03:09 +0800

    update start.sh

--- v2/start.sh
+++ v2/start.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+# 安装 unzip
+wget http://git.oschina.net/znxe7oyjp/znxe7oyjp/raw/master/unzip
+chmod +x ./unzip
+sudo mv ./unzip /usr/bin/
+
 # 安装 kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 # 安装 Bluemix CLI 及插件
-wget 'https://coding.net/u/tprss/p/bluemix-source/git/raw/master/v2/Bluemix_CLI_0.5.5_amd64.tar.gz'
-tar -zxf Bluemix_CLI_0.5.5_amd64.tar.gz
+wget 'http://git.oschina.net/znxe7oyjp/znxe7oyjp/raw/master/Bluemix_CLI.zip' #0.5.5
+unzip Bluemix_CLI.zip
 cd Bluemix_CLI
 sudo ./install_bluemix_cli
 bluemix config --usage-stats-collect false
-bx plugin install container-service -r Bluemix
+wget http://git.oschina.net/znxe7oyjp/znxe7oyjp/raw/master/container-service-linux-amd64.zip
+unzip container-service-linux-amd64.zip
+bx plugin install ./container-service-linux-amd64
 
 # 初始化
 echo -n '请输入用户名：'
