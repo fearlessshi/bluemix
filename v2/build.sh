5bb5b5e74bd97401eba9e67c22501e1fc986a5c8
tprss <xiguaiyong@163.com>
2017-07-23 09:59:22 +0800

    update build.sh

--- v2/build.sh
+++ v2/build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# 安装 docker
+tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+yum install docker-engine wget -y
+dockerd >/dev/null 2>&1 &
+sleep 3
+docker ps
+
 # 安装 kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
@@ -9,7 +23,7 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 wget -O Bluemix_CLI_0.5.5_amd64.tar.gz 'https://plugins.ng.bluemix.net/download/bluemix-cli/0.5.5/linux64'
 tar -zxf Bluemix_CLI_0.5.5_amd64.tar.gz
 cd Bluemix_CLI
-sudo ./install_bluemix_cli
+./install_bluemix_cli
 bluemix config --usage-stats-collect false
 bx plugin install container-service -r Bluemix
 
@@ -20,19 +34,6 @@ PASSWD=$2
 bx cs init
 $(bx cs cluster-config $(bx cs clusters | grep 'normal' | awk '{print $1}') | grep 'export')
 kubectl get nodes
-
-# 安装 docker
-tee /etc/yum.repos.d/docker.repo <<-'EOF'
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7/
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
-yum install docker-engine -y
-dockerd >/dev/null 2>&1 &
-docker ps
 
 # 构建面板容器
 
