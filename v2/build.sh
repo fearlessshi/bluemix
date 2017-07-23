acfba811e9980b8675c2f0ef0919a44effa23c4e
tprss <xiguaiyong@163.com>
2017-07-23 16:58:59 +0800

    update build.sh

--- v2/build.sh
+++ v2/build.sh
@@ -36,5 +36,11 @@ $(bx cs cluster-config $(bx cs clusters | grep 'normal' | awk '{print $1}') | gr
 kubectl get nodes
 
 # 构建面板容器
+cat << _EOF_ > Dockerfile
+FROM alpine:latest
+RUN curl -Lo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+RUN chmod +x /usr/local/bin/kubectl
+RUN curl -Lo Bluemix_CLI_0.5.5_amd64.tar.gz 'https://plugins.ng.bluemix.net/download/bluemix-cli/0.5.5/linux64'; tar -zxf Bluemix_CLI_0.5.5_amd64.tar.gz; cd Bluemix_CLI; ./install_bluemix_cli; cd ..; rm -rf  Bluemix_CLI
+RUN 
 
 # 创建面板运行环境
