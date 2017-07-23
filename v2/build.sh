a6a4632a7b8690588bfb724d9d060a5bef801b5e
tprss <xiguaiyong@163.com>
2017-07-23 22:01:46 +0800

    update build.sh

--- v2/build.sh
+++ v2/build.sh
@@ -37,15 +37,11 @@ kubectl get nodes
 
 # 构建面板容器
 cat << _EOF_ > Dockerfile
-FROM centos:centos7
+FROM alpine:latest
+RUN apk add --update curl
 RUN curl -Lo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 RUN chmod +x /usr/local/bin/kubectl
-RUN curl -Lo Bluemix_CLI_0.5.5_amd64.tar.gz 'https://plugins.ng.bluemix.net/download/bluemix-cli/0.5.5/linux64'; tar -zxf Bluemix_CLI_0.5.5_amd64.tar.gz; cd Bluemix_CLI; ./install_bluemix_cli; cd ..; rm -rf  Bluemix_CLI*
-RUN bluemix config --usage-stats-collect false
-RUN bx plugin install container-service -r Bluemix
-RUN (echo 1) | bx login -a https://api.ng.bluemix.net -u $USERNAME -p $PASSWD
-RUN bx cs init
-RUN $(bx cs cluster-config $(bx cs clusters | grep 'normal' | awk '{print $1}') | grep 'export')
+ADD 
 RUN kubectl get nodes
 CMD kubectl proxy --address='0.0.0.0' --accept-hosts '.*'
 _EOF_
