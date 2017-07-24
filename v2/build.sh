d189def9132cfb8b9398389e26e1a3f762ce156a
tprss <xiguaiyong@163.com>
2017-07-25 00:28:21 +0800

    update build.sh

--- v2/build.sh
+++ v2/build.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
 # 安装 docker
-tee /etc/yum.repos.d/docker.repo <<-'EOF'
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7/
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
-sed -i 's/enabled=1/enabled=0/' /etc/yum/pluginconf.d/fastestmirror.conf
-yum install docker-engine wget -y
+yum install -y yum-utils device-mapper-persistent-data lvm2 wget
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce -y
 dockerd >/dev/null 2>&1 &
 sleep 3
 docker ps
