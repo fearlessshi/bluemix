abfcc84b09c7bb1404d19e1140879a1ebc1fa7fe
tprss <xiguaiyong@163.com>
2017-09-03 17:34:47 +0800

    update start.sh

--- v2/start.sh
+++ v2/start.sh
@@ -28,6 +28,7 @@ echo -n '请输入密码：'
 read -s PASSWD
 echo -e '\n'
 (echo 1;echo 1) | bx login -a https://api.ng.bluemix.net -u $USERNAME -p $PASSWD
+bx target --cf
 bx cs init
 $(bx cs cluster-config $(bx cs clusters | grep 'normal' | awk '{print $1}') | grep 'export')
 PPW=$(openssl rand -base64 12 | md5sum | head -c12)
