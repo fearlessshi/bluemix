11f028e94b35190811d0623479afd0424133b840
shiggg <shiggg@gmail.com>
2018-02-08 04:26:20 +0800

    update build.sh

--- v2/build.sh
+++ v2/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 # 安装 docker
 yum install -y yum-utils device-mapper-persistent-data lvm2 wget openssl
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce -y
 dockerd >/dev/null 2>&1 &
 sleep 3
+
 # 安装 kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
@@ -17,6 +19,7 @@ cd Bluemix_CLI
 ./install_bluemix_cli
 bluemix config --usage-stats-collect false
 bx plugin install container-service -r Bluemix
+
 # 初始化
 AKN=$1
 AK=$(echo $2 | base64 -d)
@@ -31,29 +34,36 @@ export BLUEMIX_API_KEY=$AK
 bx cs init
 $(bx cs cluster-config $(bx cs clusters | grep 'normal' | awk '{print $1}') | grep 'export')
 bx iam api-key-delete $AKN
+
 # 初始化镜像库
 bx plugin install container-registry -r Bluemix
 bx cr login
 for name in $(bx cr namespace-list | grep del_); do (echo y) | bx cr namespace-rm $name; done
 NS=del_$(openssl rand -base64 16 | md5sum | head -c16)
 bx cr namespace-add $NS
+
 # 构建面板容器
 cp /root/.bluemix/plugins/container-service/clusters/*/*.yml ./config
 cp /root/.bluemix/plugins/container-service/clusters/*/*.pem ./
 PEM=$(basename $(ls /root/.bluemix/plugins/container-service/clusters/*/*.pem))
+
 wget -O caddy.tar.gz https://caddyserver.com/download/linux/amd64
 tar -zxf caddy.tar.gz
 chmod +x ./caddy
+
 cp /usr/local/bin/kubectl ./
+
 cat << _EOF_ > Caddyfile
 0.0.0.0:80
 gzip
 proxy /$PPW/ 127.0.0.1:8001
 _EOF_
+
 cat << _EOF_ > run.sh
 kubectl proxy --accept-hosts '.*' --api-prefix=/$PPW/ &
 caddy -conf /etc/caddy/Caddyfile
 _EOF_
+
 cat << _EOF_ > Dockerfile
 FROM alpine:latest
 RUN apk add --update ca-certificates
@@ -67,14 +77,17 @@ ADD Caddyfile /etc/caddy/
 ADD run.sh /root/
 CMD sh /root/run.sh
 _EOF_
+
 docker build -t registry.${REGION}.bluemix.net/$NS/kube .
 while ! bx cr image-list | grep -q "registry.${REGION}.bluemix.net/$NS/kube"
 do
-    sudo docker push registry.${REGION}.bluemix.net/$NS/kube
+    docker push registry.${REGION}.bluemix.net/$NS/kube
 done
+
 # 创建面板运行环境
 kubectl run kube --image=registry.${REGION}.bluemix.net/$NS/kube --port=80
 kubectl expose deployment kube --type=LoadBalancer --name=kube --external-ip $IP
+
 # 构建 SS 容器
 cat << _EOF_ >Dockerfile
 FROM easypi/shadowsocks-libev
@@ -85,8 +98,9 @@ _EOF_
 docker build -t registry.${REGION}.bluemix.net/$NS/ss .
 while ! bx cr image-list | grep -q "registry.${REGION}.bluemix.net/$NS/ss"
 do
-    docker push registry.${REGION}.bluemix.net/$NS/ss
+    sudo docker push registry.${REGION}.bluemix.net/$NS/ss
 done
+
 # 创建 BBR 构建文件
 cat << _EOF_ > bbr.yaml
 apiVersion: extensions/v1beta1
