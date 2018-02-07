ff4725606c39ae4015513763e2f7b305840cc889
shiggg <shiggg@gmail.com>
2018-02-08 05:09:05 +0800

    update build.sh

--- v2/build.sh
+++ v2/build.sh
@@ -78,10 +78,10 @@ ADD run.sh /root/
 CMD sh /root/run.sh
 _EOF_
 
-docker build -t root/registry.${REGION}.bluemix.net/$NS/kube:latest .
+docker build -t registry.${REGION}.bluemix.net/$NS/kube:latest .
 while ! bx cr image-list | grep -q "registry.${REGION}.bluemix.net/$NS/kube:latest"
 do
-    docker push root/registry.${REGION}.bluemix.net/$NS/kube:latest
+    docker push registry.${REGION}.bluemix.net/$NS/kube:latest
 done
 
 # 创建面板运行环境
@@ -95,10 +95,10 @@ ENV SERVER_PORT 443
 ENV METHOD aes-256-cfb
 ENV PASSWORD $SPW
 _EOF_
-docker build -t root/registry.${REGION}.bluemix.net/$NS/ss:latest .
+docker build -t registry.${REGION}.bluemix.net/$NS/ss:latest .
 while ! bx cr image-list | grep -q "registry.${REGION}.bluemix.net/$NS/ss:latest"
 do
-    docker push root/registry.${REGION}.bluemix.net/$NS/ss:latest
+    docker push registry.${REGION}.bluemix.net/$NS/ss:latest
 done
 
 # 创建 BBR 构建文件
