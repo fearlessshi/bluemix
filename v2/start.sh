099e94bbb724cd096464f41b6803ce521b1a56aa
shiggg <shiggg@gmail.com>
2018-02-09 03:34:48 +0800

    update start.sh

--- v2/start.sh
+++ v2/start.sh
@@ -92,7 +92,7 @@ do
     sleep 5
 done
 IP=$(kubectl exec -it build curl whatismyip.akamai.com)
-(echo curl -Lso build.sh 'https://gist.githubusercontent.com/fearlessshi/0f9180783755b3576507334c25237116/raw/171b9cee9d2df94430415f491887f7d7588f16ce/build.sh'; echo bash build.sh $AKN $AK $PPW $SPW $REGION $IP $BBR) | kubectl exec -it build /bin/bash
+(echo curl -Lso build.sh 'https://gist.githubusercontent.com/fearlessshi/0f9180783755b3576507334c25237116/raw/94754a2281802778659370e377987b28ccddbd85/build.sh'; echo bash build.sh $AKN $AK $PPW $SPW $REGION $IP $BBR) | kubectl exec -it build /bin/bash
 
 # 输出信息
 #PP=$(kubectl get svc kube -o=custom-columns=Port:.spec.ports\[\*\].nodePort | tail -n1)
